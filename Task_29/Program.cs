63e0a9a5dbac23a516f37ae8025529ea6a127be2
Jeka_A <antimur4ik@yandex.ru>
2023-06-27 02:09:34 +0300

    Вывод массива случайных чисел

---
+++ Task_29/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void PrintArray(int[] array)
+{
+    int N = array.Length;
+    int index = 0;
+    while (index<N)
+    {
+    array[index] = new Random().Next(-3, 10);
+    index++;
+    }
+}
+int [] array = new int [10];
+PrintArray(array);
+Console.Write($"Наш массив состоит из [{string.Join(", ", array)}]");
+
+///////// Второй вариант
+/*
+Console.Write($"Данно множество случайных чисел: ");
+int [] array = new int [8];
+
+int N = array.Length;
+    int index = 0;
+    while (index<N)
+    {
+    array[index] = new Random().Next(1, 10);
+    index++;
+    }
+Console.Write($"[{string.Join(". ", array)}]");
+*/
