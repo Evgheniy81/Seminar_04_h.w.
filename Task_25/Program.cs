48893e14cca005865d2eae1c6570934ac6a6d49c
Jeka_A <antimur4ik@yandex.ru>
2023-06-26 21:35:05 +0300

    Цикл для возведения 'A' в степень 'B'.

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,8 +3,23 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+/*
 int A = 3;
 int B = 5;
+int C = A;
+int i = 1;
+while (i < B)
+{
+    C = C*A;
+    i++;
+}
+Console.Write($"{A} в степени {B} = {C}");
+*/
+////// 
+Console.Write($"Введите число A => ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите число B => ");
+int B = Convert.ToInt32(Console.ReadLine());
 int C = A;
 int i = 1;
 while (i < B)
